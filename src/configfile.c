9f8f15cb0f9b40eb9bf5087312810125b6b95eda
Marcus Hufvudsson <mh@protohuf.com>
2023-09-02 21:07:36 +0200

    Refactored configfile to use config data struct

--- src/configfile.c
+++ src/configfile.c
@@ -5,24 +5,7 @@
 #include "portsentry.h"
 #include "portsentry_io.h"
 #include "portsentry_util.h"
-
-extern char gblKillRoute[MAXBUF];
-extern char gblKillHostsDeny[MAXBUF];
-extern char gblKillRunCmd[MAXBUF];
-extern char gblDetectionType[MAXBUF];
-extern char gblPorts[MAXBUF];
-extern char gblAdvancedExclude[MAXBUF];
-extern char gblPortBanner[MAXBUF];
-
-extern char gblBlockedFile[PATH_MAX];
-extern char gblHistoryFile[PATH_MAX];
-extern char gblIgnoreFile[PATH_MAX];
-
-extern int gblBlockTCP;
-extern int gblBlockUDP;
-extern int gblRunCmdFirst;
-extern int gblResolveHost;
-extern int gblConfigTriggerCount;
+#include "config_data.h"
 
 static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line);
 static char *skipSpaceAndTab(char *buffer);
@@ -37,22 +20,11 @@ int readConfigFile(void) {
   size_t keySize, line = 0;
   ssize_t valueSize;
 
-  // FIXME: Validate that gblDetectionType is a valid type
+  // FIXME: Validate that configData.gblDetectionType is a valid type
 
   /* Set defaults */
-  bzero(gblKillRoute, MAXBUF);
-  bzero(gblKillHostsDeny, MAXBUF);
-  bzero(gblKillRunCmd, MAXBUF);
-  bzero(gblPorts, MAXBUF);
-  bzero(gblAdvancedExclude, MAXBUF);
-  bzero(gblPortBanner, MAXBUF);
-
-  bzero(gblBlockedFile, PATH_MAX);
-  bzero(gblHistoryFile, PATH_MAX);
-  bzero(gblIgnoreFile, PATH_MAX);
-
-  if (strncmp(gblDetectionType, "atcp", 4) == 0 || strncmp(gblDetectionType, "audp", 4) == 0) {
-    strcpy(gblPorts, "1024");
+  if (strncmp(configData.gblDetectionType, "atcp", 4) == 0 || strncmp(configData.gblDetectionType, "audp", 4) == 0) {
+    strcpy(configData.gblPorts, "1024");
   }
 
   if ((config = fopen(CONFIG_FILE, "r")) == NULL) {
@@ -107,13 +79,13 @@ int readConfigFile(void) {
 
 
   /* Add implied config file entries */
-  if (strncmp(gblDetectionType, "atcp", 4) == 0) {
-    if (strlen(gblPorts) == 0) {
-      snprintf(gblPorts, MAXBUF, "%d", ADVANCED_MODE_PORT_TCP);
+  if (strncmp(configData.gblDetectionType, "atcp", 4) == 0) {
+    if (strlen(configData.gblPorts) == 0) {
+      snprintf(configData.gblPorts, MAXBUF, "%d", ADVANCED_MODE_PORT_TCP);
     }
-  } else if (strncmp(gblDetectionType, "audp", 4) == 0) {
-    if (strlen(gblPorts) == 0) {
-      snprintf(gblPorts, MAXBUF, "%d", ADVANCED_MODE_PORT_UDP);
+  } else if (strncmp(configData.gblDetectionType, "audp", 4) == 0) {
+    if (strlen(configData.gblPorts) == 0) {
+      snprintf(configData.gblPorts, MAXBUF, "%d", ADVANCED_MODE_PORT_UDP);
     }
   }
 
@@ -125,138 +97,138 @@ int readConfigFile(void) {
 
 static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line) {
 #ifdef DEBUG
-    Log("debug: setConfiguration: %s keySize: %u valueSize: %d gblDetectionType: %s", buffer, keySize, valueSize, gblDetectionType);
+    Log("debug: setConfiguration: %s keySize: %u valueSize: %d configData.gblDetectionType: %s", buffer, keySize, valueSize, configData.gblDetectionType);
 #endif
 
   if (strncmp(buffer, "BLOCK_TCP", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      gblBlockTCP = TRUE;
+      configData.gblBlockTCP = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      gblBlockTCP = FALSE;
+      configData.gblBlockTCP = FALSE;
     } else {
       Log("adminalert: ERROR: Invalid config file entry for BLOCK_TCP\n");
       exit(1);
     }
   } else if (strncmp(buffer, "BLOCK_UDP", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      gblBlockUDP = TRUE;
+      configData.gblBlockUDP = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      gblBlockUDP = FALSE;
+      configData.gblBlockUDP = FALSE;
     } else {
       Log("adminalert: ERROR: Invalid config file entry for BLOCK_UDP\n");
       exit(1);
     }
   } else if (strncmp(buffer, "RESOLVE_HOST", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      gblResolveHost = TRUE;
+      configData.gblResolveHost = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      gblResolveHost = FALSE;
+      configData.gblResolveHost = FALSE;
     } else {
       Log("adminalert: ERROR: Invalid config file entry for RESOLVE_HOST\n");
       exit(1);
     }
   } else if (strncmp(buffer, "SCAN_TRIGGER", keySize) == 0) {
-    gblConfigTriggerCount = getLong(ptr);
+    configData.gblConfigTriggerCount = getLong(ptr);
 
-    if (gblConfigTriggerCount < 0) {
+    if (configData.gblConfigTriggerCount < 0) {
       Log("adminalert: ERROR: Invalid config file entry for SCAN_TRIGGER\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_ROUTE", keySize) == 0) {
-    if (copyPrintableString(ptr, gblKillRoute, MAXBUF) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblKillRoute, MAXBUF) == FALSE) {
       Log("adminalert: ERROR: Unable to copy kill route\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_HOSTS_DENY", keySize) == 0) {
-    if (copyPrintableString(ptr, gblKillHostsDeny, MAXBUF) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblKillHostsDeny, MAXBUF) == FALSE) {
       Log("adminalert: ERROR: Unable to copy kill hosts deny\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_RUN_CMD", keySize) == 0) {
-    if (copyPrintableString(ptr, gblKillRunCmd, MAXBUF) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblKillRunCmd, MAXBUF) == FALSE) {
       Log("adminalert: ERROR: Unable to copy kill run command\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_RUN_CMD_FIRST", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      gblRunCmdFirst = TRUE;
+      configData.gblRunCmdFirst = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      gblRunCmdFirst = FALSE;
+      configData.gblRunCmdFirst = FALSE;
     } else {
       Log("adminalert: ERROR: Invalid config file entry for KILL_RUN_CMD_FIRST\n");
       exit(1);
     }
   } else if (strncmp(buffer, "BLOCKED_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, gblBlockedFile, PATH_MAX) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblBlockedFile, PATH_MAX) == FALSE) {
       Log("adminalert: ERROR: Unable to copy blocked file path\n");
       exit(1);
     }
-    if (strlen(gblBlockedFile) < (PATH_MAX - 5)) {
-      strncat(gblBlockedFile, ".", 1);
-      strncat(gblBlockedFile, gblDetectionType, 4);
+    if (strlen(configData.gblBlockedFile) < (PATH_MAX - 5)) {
+      strncat(configData.gblBlockedFile, ".", 1);
+      strncat(configData.gblBlockedFile, configData.gblDetectionType, 4);
     } else {
-      Log("adminalert: ERROR: Blocked filename is too long to append detection type file extension: %s\n", gblBlockedFile);
+      Log("adminalert: ERROR: Blocked filename is too long to append detection type file extension: %s\n", configData.gblBlockedFile);
       exit(1);
     }
 
-    if (testFileAccess(gblBlockedFile, "w") == FALSE) {
-      Log("adminalert: ERROR: Unable to open block file for writing: %s\n", gblBlockedFile);
+    if (testFileAccess(configData.gblBlockedFile, "w") == FALSE) {
+      Log("adminalert: ERROR: Unable to open block file for writing: %s\n", configData.gblBlockedFile);
       exit(1);
     }
   } else if (strncmp(buffer, "HISTORY_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, gblHistoryFile, PATH_MAX) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblHistoryFile, PATH_MAX) == FALSE) {
       Log("adminalert: ERROR: Unable to copy history file path\n");
       exit(1);
     }
   } else if (strncmp(buffer, "IGNORE_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, gblIgnoreFile, PATH_MAX) == FALSE) {
+    if (copyPrintableString(ptr, configData.gblIgnoreFile, PATH_MAX) == FALSE) {
       Log("adminalert: ERROR: Unable to copy ignore file path\n");
       exit(1);
     }
   } else if (strncmp(buffer, "TCP_PORTS", keySize) == 0) {
-    if (strncmp(gblDetectionType, "tcp", 3) == 0 || strncmp(gblDetectionType, "stcp", 4) == 0) {
-      if (copyPrintableString(ptr, gblPorts, MAXBUF) == FALSE) {
+    if (strncmp(configData.gblDetectionType, "tcp", 3) == 0 || strncmp(configData.gblDetectionType, "stcp", 4) == 0) {
+      if (copyPrintableString(ptr, configData.gblPorts, MAXBUF) == FALSE) {
       Log("adminalert: ERROR: Unable to copy TCP ports\n");
       exit(1);
       }
     }
   } else if (strncmp(buffer, "UDP_PORTS", keySize) == 0) {
-    if ((strncmp(gblDetectionType, "udp", 3) == 0 || strncmp(gblDetectionType, "sudp", 4) == 0)) {
-      if (copyPrintableString(ptr, gblPorts, MAXBUF) == FALSE) {
+    if ((strncmp(configData.gblDetectionType, "udp", 3) == 0 || strncmp(configData.gblDetectionType, "sudp", 4) == 0)) {
+      if (copyPrintableString(ptr, configData.gblPorts, MAXBUF) == FALSE) {
         Log("adminalert: ERROR: Unable to copy UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_PORTS_TCP", keySize) == 0) {
-    if (strncmp(gblDetectionType, "atcp", 4) == 0) {
-      if (copyPrintableString(ptr, gblPorts, MAXBUF) == FALSE) {
+    if (strncmp(configData.gblDetectionType, "atcp", 4) == 0) {
+      if (copyPrintableString(ptr, configData.gblPorts, MAXBUF) == FALSE) {
         Log("adminalert: ERROR: Unable to copy advanced TCP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_PORTS_UDP", keySize) == 0) {
-    if (strncmp(gblDetectionType, "audp", 4) == 0) {
-      if (copyPrintableString(ptr, gblPorts, MAXBUF) == FALSE) {
+    if (strncmp(configData.gblDetectionType, "audp", 4) == 0) {
+      if (copyPrintableString(ptr, configData.gblPorts, MAXBUF) == FALSE) {
         Log("adminalert: ERROR: Unable to copy advanced UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_EXCLUDE_TCP", keySize) == 0) {
-    if (strncmp(gblDetectionType, "atcp", 4) == 0) {
-      if (copyPrintableString(ptr, gblAdvancedExclude, MAXBUF) == FALSE) {
+    if (strncmp(configData.gblDetectionType, "atcp", 4) == 0) {
+      if (copyPrintableString(ptr, configData.gblAdvancedExclude, MAXBUF) == FALSE) {
         Log("adminalert: ERROR: Unable to copy advanced exclude TCP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_EXCLUDE_UDP", keySize) == 0) {
-    if (strncmp(gblDetectionType, "audp", 4) == 0) {
-      if (copyPrintableString(ptr, gblAdvancedExclude, MAXBUF) == FALSE) {
+    if (strncmp(configData.gblDetectionType, "audp", 4) == 0) {
+      if (copyPrintableString(ptr, configData.gblAdvancedExclude, MAXBUF) == FALSE) {
         Log("adminalert: ERROR: Unable to copy advanced exclude UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "PORT_BANNER", keySize) == 0) {
-    copyPrintableString(ptr, gblPortBanner, MAXBUF);
+    copyPrintableString(ptr, configData.gblPortBanner, MAXBUF);
   } else {
     Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
     exit(1);
@@ -264,24 +236,24 @@ static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t va
 }
 
 void validateConfig(void) {
-  // FIXME: When validating gblPorts / gblExcludePorts, make sure the combination w/ gblDetectionType is valid
+  // FIXME: When validating configData.gblPorts / configData.gblExcludePorts, make sure the combination w/ configData.gblDetectionType is valid
   // Advanced Ports: If not set, default to 1024
   // Wait with this function until the config file (and cmdline) is stored in a struct fulle parsed
 #ifdef DEBUG
-  Log("debug: gblBlockTCP: %d\n", gblBlockTCP);
-  Log("debug: gblBlockUDP: %d\n", gblBlockUDP);
-  Log("debug: gblResolveHost: %d\n", gblResolveHost);
-  Log("debug: gblConfigTriggerCount: %u\n", gblConfigTriggerCount);
-  Log("debug: gblKillRoute: %s\n", gblKillRoute);
-  Log("debug: gblKillHostsDeny: %s\n", gblKillHostsDeny);
-  Log("debug: gblKillRunCmd: %s\n", gblKillRunCmd);
-  Log("debug: gblRunCmdFirst: %d\n", gblRunCmdFirst);
-  Log("debug: gblPorts: %s\n", gblPorts);
-  Log("debug: gblAdvancedExclude: %s\n", gblAdvancedExclude);
-  Log("debug: gblPortBanner: %s\n", gblPortBanner);
-  Log("debug: gblBlockedFile: %s\n", gblBlockedFile);
-  Log("debug: gblHistoryFile: %s\n", gblHistoryFile);
-  Log("debug: gblIgnoreFile: %s\n", gblIgnoreFile);
+  Log("debug: configData.gblBlockTCP: %d\n", configData.gblBlockTCP);
+  Log("debug: configData.gblBlockUDP: %d\n", configData.gblBlockUDP);
+  Log("debug: configData.gblResolveHost: %d\n", configData.gblResolveHost);
+  Log("debug: configData.gblConfigTriggerCount: %u\n", configData.gblConfigTriggerCount);
+  Log("debug: configData.gblKillRoute: %s\n", configData.gblKillRoute);
+  Log("debug: configData.gblKillHostsDeny: %s\n", configData.gblKillHostsDeny);
+  Log("debug: configData.gblKillRunCmd: %s\n", configData.gblKillRunCmd);
+  Log("debug: configData.gblRunCmdFirst: %d\n", configData.gblRunCmdFirst);
+  Log("debug: configData.gblPorts: %s\n", configData.gblPorts);
+  Log("debug: configData.gblAdvancedExclude: %s\n", configData.gblAdvancedExclude);
+  Log("debug: configData.gblPortBanner: %s\n", configData.gblPortBanner);
+  Log("debug: configData.gblBlockedFile: %s\n", configData.gblBlockedFile);
+  Log("debug: configData.gblHistoryFile: %s\n", configData.gblHistoryFile);
+  Log("debug: configData.gblIgnoreFile: %s\n", configData.gblIgnoreFile);
 #endif
 }
 
