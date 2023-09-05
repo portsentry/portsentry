06c0ee92ea9cdb5f6e86cf68652e85d26565f40d
Marcus Hufvudsson <mh@protohuf.com>
2023-09-05 22:46:56 +0200

    refactored config file parser to load data in file config struct for later merging

--- src/configfile.c
+++ src/configfile.c
@@ -7,14 +7,14 @@
 #include "portsentry_util.h"
 #include "config_data.h"
 
-static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line);
+static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line, struct ConfigData *fileConfig, struct ConfigData *cmdlineConfig);
 static char *skipSpaceAndTab(char *buffer);
 static size_t getKeySize(char *buffer);
 static void stripTrailingSpace(char *buffer);
 static ssize_t getSizeToQuote(char *buffer);
-void validateConfig(void);
+void validateConfig(struct ConfigData *fileConfig);
 
-struct ConfigData readConfigFile(void) {
+struct ConfigData readConfigFile(struct ConfigData *cmdlineConfig) {
   struct ConfigData fileConfig;
   FILE *config;
   char buffer[MAXBUF], *ptr;
@@ -24,12 +24,12 @@ struct ConfigData readConfigFile(void) {
   ResetConfigData(&fileConfig);
 
   /* Set defaults */
-  if (configData.sentryMode == SENTRY_MODE_ATCP || configData.sentryMode == SENTRY_MODE_AUDP) {
+  if (cmdlineConfig->sentryMode == SENTRY_MODE_ATCP || cmdlineConfig->sentryMode == SENTRY_MODE_AUDP) {
     strcpy(fileConfig.ports, "1024");
   }
 
-  if ((config = fopen(configData.configFile, "r")) == NULL) {
-    Log("adminalert: ERROR: Cannot open config file: %s.\n", configData.configFile);
+  if ((config = fopen(cmdlineConfig->configFile, "r")) == NULL) {
+    fprintf(stderr, "Cannot open config file: %s.\n", cmdlineConfig->configFile);
     Exit(EXIT_FAILURE);
   }
 
@@ -43,7 +43,7 @@ struct ConfigData readConfigFile(void) {
     stripTrailingSpace(buffer);
 
     if ((keySize = getKeySize(buffer)) == 0) {
-      Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
+      fprintf(stderr, "Invalid config file entry at line %lu\n", line);
       fclose(config);
       Exit(EXIT_FAILURE);
     }
@@ -52,7 +52,7 @@ struct ConfigData readConfigFile(void) {
     ptr = skipSpaceAndTab(ptr);
 
     if (*ptr != '=') {
-      Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
+      fprintf(stderr, "Invalid config file entry at line %lu\n", line);
       fclose(config);
       Exit(EXIT_FAILURE);
     }
@@ -61,198 +61,200 @@ struct ConfigData readConfigFile(void) {
     ptr = skipSpaceAndTab(ptr);
 
     if (*ptr != '"') {
-      Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
+      fprintf(stderr, "Invalid config file entry at line %lu\n", line);
       fclose(config);
       Exit(EXIT_FAILURE);
     }
     ptr++;
 
     if ((valueSize = getSizeToQuote(ptr)) == ERROR) {
-      Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
+      fprintf(stderr, "Invalid config file entry at line %lu\n", line);
       fclose(config);
       Exit(EXIT_FAILURE);
     }
 
-    setConfiguration(buffer, keySize, ptr, valueSize, line);
+    setConfiguration(buffer, keySize, ptr, valueSize, line, &fileConfig, cmdlineConfig);
   }
 
   fclose(config);
 
 
   /* Add implied config file entries */
-  if (configData.sentryMode == SENTRY_MODE_ATCP) {
-    if (strlen(configData.ports) == 0) {
-      snprintf(configData.ports, MAXBUF, "%d", ADVANCED_MODE_PORT_TCP);
+  if (cmdlineConfig->sentryMode == SENTRY_MODE_ATCP) {
+    if (strlen(fileConfig.ports) == 0) {
+      snprintf(fileConfig.ports, MAXBUF, "%d", ADVANCED_MODE_PORT_TCP);
     }
-  } else if (configData.sentryMode == SENTRY_MODE_AUDP) {
-    if (strlen(configData.ports) == 0) {
-      snprintf(configData.ports, MAXBUF, "%d", ADVANCED_MODE_PORT_UDP);
+  } else if (cmdlineConfig->sentryMode == SENTRY_MODE_AUDP) {
+    if (strlen(fileConfig.ports) == 0) {
+      snprintf(fileConfig.ports, MAXBUF, "%d", ADVANCED_MODE_PORT_UDP);
     }
   }
 
   /* Make sure config is valid */
-  validateConfig();
+  validateConfig(&fileConfig);
+
+  return fileConfig;
 }
 
-static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line) {
+static void setConfiguration(char *buffer, size_t keySize, char *ptr, ssize_t valueSize, const size_t line, struct ConfigData *fileConfig, struct ConfigData *cmdlineConfig) {
 #ifdef DEBUG
-    Log("debug: setConfiguration: %s keySize: %u valueSize: %d configData.sentryMode: %s", buffer, keySize, valueSize, GetSentryModeString(configData.sentryMode));
+    fprintf(stderr, "setConfiguration: %s keySize: %lu valueSize: %ld sentryMode: %s\n", buffer, keySize, valueSize, GetSentryModeString(cmdlineConfig->sentryMode));
 #endif
 
   if (strncmp(buffer, "BLOCK_TCP", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      configData.blockTCP = TRUE;
+      fileConfig->blockTCP = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      configData.blockTCP = FALSE;
+      fileConfig->blockTCP = FALSE;
     } else {
-      Log("adminalert: ERROR: Invalid config file entry for BLOCK_TCP\n");
+      fprintf(stderr, "Invalid config file entry for BLOCK_TCP\n");
       exit(1);
     }
   } else if (strncmp(buffer, "BLOCK_UDP", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      configData.blockUDP = TRUE;
+      fileConfig->blockUDP = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      configData.blockUDP = FALSE;
+      fileConfig->blockUDP = FALSE;
     } else {
-      Log("adminalert: ERROR: Invalid config file entry for BLOCK_UDP\n");
+      fprintf(stderr, "Invalid config file entry for BLOCK_UDP\n");
       exit(1);
     }
   } else if (strncmp(buffer, "RESOLVE_HOST", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      configData.resolveHost = TRUE;
+      fileConfig->resolveHost = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      configData.resolveHost = FALSE;
+      fileConfig->resolveHost = FALSE;
     } else {
-      Log("adminalert: ERROR: Invalid config file entry for RESOLVE_HOST\n");
+      fprintf(stderr, "Invalid config file entry for RESOLVE_HOST\n");
       exit(1);
     }
   } else if (strncmp(buffer, "SCAN_TRIGGER", keySize) == 0) {
-    configData.configTriggerCount = getLong(ptr);
+    fileConfig->configTriggerCount = getLong(ptr);
 
-    if (configData.configTriggerCount < 0) {
-      Log("adminalert: ERROR: Invalid config file entry for SCAN_TRIGGER\n");
+    if (fileConfig->configTriggerCount < 0) {
+      fprintf(stderr, "Invalid config file entry for SCAN_TRIGGER\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_ROUTE", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.killRoute, MAXBUF) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy kill route\n");
+    if (copyPrintableString(ptr, fileConfig->killRoute, MAXBUF) == FALSE) {
+      fprintf(stderr, "Unable to copy kill route\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_HOSTS_DENY", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.killHostsDeny, MAXBUF) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy kill hosts deny\n");
+    if (copyPrintableString(ptr, fileConfig->killHostsDeny, MAXBUF) == FALSE) {
+      fprintf(stderr, "Unable to copy kill hosts deny\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_RUN_CMD", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.killRunCmd, MAXBUF) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy kill run command\n");
+    if (copyPrintableString(ptr, fileConfig->killRunCmd, MAXBUF) == FALSE) {
+      fprintf(stderr, "Unable to copy kill run command\n");
       exit(1);
     }
   } else if (strncmp(buffer, "KILL_RUN_CMD_FIRST", keySize) == 0) {
     if (strncmp(ptr, "1", valueSize) == 0) {
-      configData.runCmdFirst = TRUE;
+      fileConfig->runCmdFirst = TRUE;
     } else if (strncmp(ptr, "0", valueSize) == 0) {
-      configData.runCmdFirst = FALSE;
+      fileConfig->runCmdFirst = FALSE;
     } else {
-      Log("adminalert: ERROR: Invalid config file entry for KILL_RUN_CMD_FIRST\n");
+      fprintf(stderr, "Invalid config file entry for KILL_RUN_CMD_FIRST\n");
       exit(1);
     }
   } else if (strncmp(buffer, "BLOCKED_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.blockedFile, PATH_MAX) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy blocked file path\n");
+    if (copyPrintableString(ptr, fileConfig->blockedFile, PATH_MAX) == FALSE) {
+      fprintf(stderr, "Unable to copy blocked file path\n");
       exit(1);
     }
-    if (strlen(configData.blockedFile) < (PATH_MAX - 5)) {
-      strncat(configData.blockedFile, ".", 1);
-      strncat(configData.blockedFile, GetSentryModeString(configData.sentryMode), 4);
+    if (strlen(fileConfig->blockedFile) < (PATH_MAX - 5)) {
+      strncat(fileConfig->blockedFile, ".", 1);
+      strncat(fileConfig->blockedFile, GetSentryModeString(cmdlineConfig->sentryMode), 4);
     } else {
-      Log("adminalert: ERROR: Blocked filename is too long to append sentry mode file extension: %s\n", configData.blockedFile);
+      fprintf(stderr, "Blocked filename is too long to append sentry mode file extension: %s\n", fileConfig->blockedFile);
       exit(1);
     }
 
-    if (testFileAccess(configData.blockedFile, "w") == FALSE) {
-      Log("adminalert: ERROR: Unable to open block file for writing: %s\n", configData.blockedFile);
+    if (testFileAccess(fileConfig->blockedFile, "w") == FALSE) {
+      fprintf(stderr, "Unable to open block file for writing: %s\n", fileConfig->blockedFile);
       exit(1);
     }
   } else if (strncmp(buffer, "HISTORY_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.historyFile, PATH_MAX) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy history file path\n");
+    if (copyPrintableString(ptr, fileConfig->historyFile, PATH_MAX) == FALSE) {
+      fprintf(stderr, "Unable to copy history file path\n");
       exit(1);
     }
   } else if (strncmp(buffer, "IGNORE_FILE", keySize) == 0) {
-    if (copyPrintableString(ptr, configData.ignoreFile, PATH_MAX) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy ignore file path\n");
+    if (copyPrintableString(ptr, fileConfig->ignoreFile, PATH_MAX) == FALSE) {
+      fprintf(stderr, "Unable to copy ignore file path\n");
       exit(1);
     }
   } else if (strncmp(buffer, "TCP_PORTS", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_TCP || configData.sentryMode == SENTRY_MODE_STCP) {
-      if (copyPrintableString(ptr, configData.ports, MAXBUF) == FALSE) {
-      Log("adminalert: ERROR: Unable to copy TCP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_TCP || cmdlineConfig->sentryMode == SENTRY_MODE_STCP) {
+      if (copyPrintableString(ptr, fileConfig->ports, MAXBUF) == FALSE) {
+      fprintf(stderr, "Unable to copy TCP ports\n");
       exit(1);
       }
     }
   } else if (strncmp(buffer, "UDP_PORTS", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_UDP || configData.sentryMode == SENTRY_MODE_SUDP) {
-      if (copyPrintableString(ptr, configData.ports, MAXBUF) == FALSE) {
-        Log("adminalert: ERROR: Unable to copy UDP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_UDP || cmdlineConfig->sentryMode == SENTRY_MODE_SUDP) {
+      if (copyPrintableString(ptr, fileConfig->ports, MAXBUF) == FALSE) {
+        fprintf(stderr, "Unable to copy UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_PORTS_TCP", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_ATCP) {
-      if (copyPrintableString(ptr, configData.ports, MAXBUF) == FALSE) {
-        Log("adminalert: ERROR: Unable to copy advanced TCP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_ATCP) {
+      if (copyPrintableString(ptr, fileConfig->ports, MAXBUF) == FALSE) {
+        fprintf(stderr, "Unable to copy advanced TCP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_PORTS_UDP", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_AUDP) {
-      if (copyPrintableString(ptr, configData.ports, MAXBUF) == FALSE) {
-        Log("adminalert: ERROR: Unable to copy advanced UDP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_AUDP) {
+      if (copyPrintableString(ptr, fileConfig->ports, MAXBUF) == FALSE) {
+        fprintf(stderr, "Unable to copy advanced UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_EXCLUDE_TCP", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_ATCP) {
-      if (copyPrintableString(ptr, configData.advancedExclude, MAXBUF) == FALSE) {
-        Log("adminalert: ERROR: Unable to copy advanced exclude TCP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_ATCP) {
+      if (copyPrintableString(ptr, fileConfig->advancedExclude, MAXBUF) == FALSE) {
+        fprintf(stderr, "Unable to copy advanced exclude TCP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "ADVANCED_EXCLUDE_UDP", keySize) == 0) {
-    if (configData.sentryMode == SENTRY_MODE_AUDP) {
-      if (copyPrintableString(ptr, configData.advancedExclude, MAXBUF) == FALSE) {
-        Log("adminalert: ERROR: Unable to copy advanced exclude UDP ports\n");
+    if (cmdlineConfig->sentryMode == SENTRY_MODE_AUDP) {
+      if (copyPrintableString(ptr, fileConfig->advancedExclude, MAXBUF) == FALSE) {
+        fprintf(stderr, "Unable to copy advanced exclude UDP ports\n");
         exit(1);
       }
     }
   } else if (strncmp(buffer, "PORT_BANNER", keySize) == 0) {
-    copyPrintableString(ptr, configData.portBanner, MAXBUF);
+    copyPrintableString(ptr, fileConfig->portBanner, MAXBUF);
   } else {
-    Log("adminalert: ERROR: Invalid config file entry at line %lu\n", line);
+    fprintf(stderr, "Invalid config file entry at line %lu\n", line);
     exit(1);
   }
 }
 
-void validateConfig(void) {
+void validateConfig(struct ConfigData *fileConfig) {
   // FIXME: When validating configData.ports / configData.excludePorts, make sure the combination w/ configData.detectionType is valid
   // Advanced Ports: If not set, default to 1024
   // Wait with this function until the config file (and cmdline) is stored in a struct fulle parsed
 #ifdef DEBUG
-  Log("debug: configData.blockTCP: %d\n", configData.blockTCP);
-  Log("debug: configData.blockUDP: %d\n", configData.blockUDP);
-  Log("debug: configData.resolveHost: %d\n", configData.resolveHost);
-  Log("debug: configData.configTriggerCount: %u\n", configData.configTriggerCount);
-  Log("debug: configData.killRoute: %s\n", configData.killRoute);
-  Log("debug: configData.killHostsDeny: %s\n", configData.killHostsDeny);
-  Log("debug: configData.killRunCmd: %s\n", configData.killRunCmd);
-  Log("debug: configData.runCmdFirst: %d\n", configData.runCmdFirst);
-  Log("debug: configData.ports: %s\n", configData.ports);
-  Log("debug: configData.advancedExclude: %s\n", configData.advancedExclude);
-  Log("debug: configData.portBanner: %s\n", configData.portBanner);
-  Log("debug: configData.blockedFile: %s\n", configData.blockedFile);
-  Log("debug: configData.historyFile: %s\n", configData.historyFile);
-  Log("debug: configData.ignoreFile: %s\n", configData.ignoreFile);
+  fprintf(stderr, "blockTCP: %d\n", fileConfig->blockTCP);
+  fprintf(stderr, "blockUDP: %d\n", fileConfig->blockUDP);
+  fprintf(stderr, "resolveHost: %d\n", fileConfig->resolveHost);
+  fprintf(stderr, "configTriggerCount: %u\n", fileConfig->configTriggerCount);
+  fprintf(stderr, "killRoute: %s\n", fileConfig->killRoute);
+  fprintf(stderr, "killHostsDeny: %s\n", fileConfig->killHostsDeny);
+  fprintf(stderr, "killRunCmd: %s\n", fileConfig->killRunCmd);
+  fprintf(stderr, "runCmdFirst: %d\n", fileConfig->runCmdFirst);
+  fprintf(stderr, "ports: %s\n", fileConfig->ports);
+  fprintf(stderr, "advancedExclude: %s\n", fileConfig->advancedExclude);
+  fprintf(stderr, "portBanner: %s\n", fileConfig->portBanner);
+  fprintf(stderr, "blockedFile: %s\n", fileConfig->blockedFile);
+  fprintf(stderr, "historyFile: %s\n", fileConfig->historyFile);
+  fprintf(stderr, "ignoreFile: %s\n", fileConfig->ignoreFile);
 #endif
 }
 
--- src/configfile.h
+++ src/configfile.h
@@ -3,4 +3,4 @@
 #define ADVANCED_MODE_PORT_TCP  1024
 #define ADVANCED_MODE_PORT_UDP  1024
 
-struct ConfigData readConfigFile(void);
+struct ConfigData readConfigFile(struct ConfigData *cmdlineConfig);
