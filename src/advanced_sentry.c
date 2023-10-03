8756634b212550adccf48df2da3f53685a4327cd
Marcus Hufvudsson <mh@protohuf.com>
2023-10-03 21:40:32 +0200

    Initial advanced sentry mode unification

---
+++ src/advanced_sentry.c
@@ -0,0 +1,204 @@
+#include <arpa/inet.h>
+#include <assert.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <poll.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "config_data.h"
+#include "connection_data.h"
+#include "io.h"
+#include "portsentry.h"
+#include "state_machine.h"
+#include "util.h"
+
+int PortSentryAdvancedStealthMode(void) {
+  int result, nfds, tcpSockfd, udpSockfd, count;
+  char target[IPMAXBUF];
+  char resolvedHost[DNSMAXBUF], *packetType;
+  char packetBuffer[IP_MAXPACKET];
+  struct sockaddr_in client;
+  struct iphdr *ip;
+  struct tcphdr *tcp;
+  struct udphdr *udp;
+  struct pollfd fds[2];
+  struct ConnectionData connectionData[MAXSOCKS];
+  struct ConnectionData *cd, tmpcd;
+  int connectionDataSize;
+  void *p;
+
+  assert(configData.sentryMode == SENTRY_MODE_ATCP || configData.sentryMode == SENTRY_MODE_AUDP);
+
+  Log("adminalert: Advanced mode will monitor first %d TCP ports and %d UDP ports", configData.tcpAdvancedPort, configData.udpAdvancedPort);
+
+  connectionDataSize = ConstructConnectionData(connectionData, MAXSOCKS);
+
+  if (configData.sentryMode == SENTRY_MODE_ATCP && configData.tcpAdvancedExcludePortsLength > 0) {
+    for (count = 0; count < configData.tcpAdvancedExcludePortsLength; count++) {
+      if (FindConnectionData(connectionData, MAXSOCKS, configData.tcpAdvancedExcludePorts[count], IPPROTO_TCP) != NULL) {
+        continue;
+      }
+
+      if (connectionDataSize >= MAXSOCKS) {
+        Log("adminalert: ERROR: TCP port count exceeds size of ConnectionData array. Aborting.");
+        return ERROR;
+      }
+
+      SetConnectionData(&connectionData[connectionDataSize], configData.tcpAdvancedExcludePorts[count], IPPROTO_TCP, FALSE);
+      connectionData[connectionDataSize].portInUse = TRUE;
+      Log("Advanced mode will manually exclude TCP port: %d ", configData.tcpAdvancedExcludePorts[count]);
+
+      connectionDataSize++;
+    }
+  } else {
+    Log("Advanced mode will manually exclude no ports");
+  }
+
+  if (configData.sentryMode == SENTRY_MODE_AUDP && configData.udpAdvancedExcludePortsLength > 0) {
+    for (count = 0; count < configData.udpAdvancedExcludePortsLength; count++) {
+      if (FindConnectionData(connectionData, MAXSOCKS, configData.udpAdvancedExcludePorts[count], IPPROTO_UDP) != NULL) {
+        continue;
+      }
+
+      if (connectionDataSize >= MAXSOCKS) {
+        Log("adminalert: ERROR: UDP port count exceeds size of ConnectionData array. Aborting.");
+        return ERROR;
+      }
+
+      SetConnectionData(&connectionData[connectionDataSize], configData.udpAdvancedExcludePorts[count], IPPROTO_UDP, FALSE);
+      connectionData[connectionDataSize].portInUse = TRUE;
+      Log("Advanced mode will manually exclude UDP port: %d ", configData.udpAdvancedExcludePorts[count]);
+
+      connectionDataSize++;
+    }
+  } else {
+    Log("Advanced mode will manually exclude no ports");
+  }
+
+  for (count = 0; count < connectionDataSize; count++) {
+    Log("adminalert: Advanced Stealth scan detection mode activated. Ignored %s port: %d", connectionData[count].port, GetProtocolString(connectionData[count].protocol));
+  }
+
+  nfds = 0;
+  if (configData.sentryMode == SENTRY_MODE_ATCP) {
+    if ((tcpSockfd = OpenRAWTCPSocket()) == ERROR) {
+      Log("adminalert: ERROR: could not open RAW TCP socket. Aborting.");
+      return (ERROR);
+    }
+
+    fds[nfds].fd = tcpSockfd;
+    fds[nfds].events = POLLIN;
+    nfds++;
+  }
+
+  if (configData.sentryMode == SENTRY_MODE_AUDP) {
+    if ((udpSockfd = OpenRAWUDPSocket()) == ERROR) {
+      Log("adminalert: ERROR: could not open RAW UDP socket. Aborting.");
+      return (ERROR);
+    }
+
+    fds[nfds].fd = udpSockfd;
+    fds[nfds].events = POLLIN;
+    nfds++;
+  }
+
+  Log("adminalert: PortSentry is now active and listening.");
+
+  for (;;) {
+    result = poll(fds, nfds, -1);
+    if (result == -1) {
+      Log("adminalert: ERROR: poll() failed. Aborting.");
+      return (ERROR);
+    } else if (result == 0) {
+      Log("adminalert: ERROR: poll() timed out. Aborting.");
+      return (ERROR);
+    }
+
+    for (count = 0; count < nfds; count++) {
+      if (fds[count].revents != POLLIN) {
+        continue;
+      }
+
+      if (PacketRead(fds[count].fd, packetBuffer, IP_MAXPACKET, &ip, &p) != TRUE)
+        continue;
+
+      memset(&client, 0, sizeof(client));
+      client.sin_family = AF_INET;
+      client.sin_addr.s_addr = ip->saddr;
+      if (ip->protocol == IPPROTO_TCP) {
+        tcp = (struct tcphdr *)p;
+        if (ntohs(tcp->dest) > configData.tcpAdvancedPort)
+          continue;
+        if ((cd = FindConnectionData(connectionData, connectionDataSize, ntohs(tcp->dest), IPPROTO_TCP)) != NULL)
+          continue;
+        client.sin_port = tcp->dest;
+      } else if (ip->protocol == IPPROTO_UDP) {
+        udp = (struct udphdr *)p;
+        if (ntohs(udp->dest) > configData.udpAdvancedPort)
+          continue;
+        if ((cd = FindConnectionData(connectionData, connectionDataSize, ntohs(udp->dest), IPPROTO_UDP)) != NULL)
+          continue;
+        client.sin_port = udp->dest;
+      }
+
+      // Since we make heavy use of the ConnectionData structure create a temporary one to hold the current data
+      SetConnectionData(&tmpcd, (ip->protocol == IPPROTO_TCP) ? ntohs(tcp->dest) : ntohs(udp->dest), ip->protocol, FALSE);
+      tmpcd.portInUse = FALSE;
+      cd = &tmpcd;
+
+      // FIXME : Do we need this?
+      if (cd->protocol == IPPROTO_TCP && (tcp->ack == 1 || tcp->rst == 1)) {
+        continue;
+      }
+
+      if (IsPortInUse(cd->port, cd->protocol) != FALSE) {
+        continue;
+      }
+
+      SafeStrncpy(target, inet_ntoa(client.sin_addr), IPMAXBUF);
+
+      if ((result = NeverBlock(target, configData.ignoreFile)) == ERROR) {
+        Log("attackalert: ERROR: cannot open ignore file %s. Blocking host anyway.", configData.ignoreFile);
+        result = FALSE;
+      } else if (result == TRUE) {
+        Log("attackalert: Host: %s found in ignore file %s, aborting actions", target, configData.ignoreFile);
+        continue;
+      }
+
+      if (CheckStateEngine(target) != TRUE) {
+        continue;
+      }
+
+      if (configData.resolveHost == TRUE) {
+        ResolveAddr((struct sockaddr *)&client, sizeof(client), resolvedHost, DNSMAXBUF);
+      } else {
+        snprintf(resolvedHost, DNSMAXBUF, "%s", target);
+      }
+
+      if (cd->protocol == IPPROTO_TCP) {
+        packetType = ReportPacketType(tcp);
+        Log("attackalert: %s from host: %s/%s to TCP port: %d", packetType, resolvedHost, target, cd->port);
+      } else {
+        Log("attackalert: UDP scan from host: %s/%s to UDP port: %d", resolvedHost, target, cd->port);
+      }
+
+      if (ip->ihl > 5)
+        Log("attackalert: Packet from host: %s/%s to %s port: %d has IP options set (detection avoidance technique).", resolvedHost, target, GetProtocolString(cd->protocol), cd->port);
+
+      if (IsBlocked(target, configData.blockedFile) == FALSE) {
+        if (DisposeTarget(target, cd->port, cd->protocol) != TRUE)
+          Log("attackalert: ERROR: Could not block host %s/%s!", resolvedHost, target);
+        else
+          WriteBlocked(target, resolvedHost, cd->port, configData.blockedFile, configData.historyFile, GetProtocolString(cd->protocol));
+      } else {
+        Log("attackalert: Host: %s/%s is already blocked Ignoring", resolvedHost, target);
+      }
+    }
+  }
+
+  close(tcpSockfd);
+  close(udpSockfd);
+}
--- src/portsentry.c
+++ src/portsentry.c
@@ -24,6 +24,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "advanced_sentry.h"
 #include "cmdline.h"
 #include "config_data.h"
 #include "configfile.h"
@@ -33,11 +34,6 @@
 #include "state_machine.h"
 #include "stealth_sentry.h"
 #include "util.h"
-
-#ifdef SUPPORT_STEALTH
-static int PortSentryAdvancedStealthModeTCP(void);
-static int PortSentryAdvancedStealthModeUDP(void);
-#endif
 
 int main(int argc, char *argv[]) {
   ParseCmdline(argc, argv);
@@ -67,284 +63,17 @@ int main(int argc, char *argv[]) {
       Log("adminalert: ERROR: could not go into PortSentry mode. Shutting down.");
       Exit(ERROR);
     }
-  }
-#ifdef SUPPORT_STEALTH
-  else if (configData.sentryMode == SENTRY_MODE_STCP || configData.sentryMode == SENTRY_MODE_SUDP) {
+  } else if (configData.sentryMode == SENTRY_MODE_STCP || configData.sentryMode == SENTRY_MODE_SUDP) {
     if (PortSentryStealthMode() == ERROR) {
       Log("adminalert: ERROR: could not go into PortSentry mode. Shutting down.");
       Exit(ERROR);
     }
-  } else if (configData.sentryMode == SENTRY_MODE_ATCP) {
-    if (PortSentryAdvancedStealthModeTCP() == ERROR) {
-      Log("adminalert: ERROR: could not go into PortSentry mode. Shutting down.");
-      Exit(ERROR);
-    }
-  } else if (configData.sentryMode == SENTRY_MODE_AUDP) {
-    if (PortSentryAdvancedStealthModeUDP() == ERROR) {
+  } else if (configData.sentryMode == SENTRY_MODE_ATCP || configData.sentryMode == SENTRY_MODE_AUDP) {
+    if (PortSentryAdvancedStealthMode() == ERROR) {
       Log("adminalert: ERROR: could not go into PortSentry mode. Shutting down.");
       Exit(ERROR);
     }
   }
-#endif
 
   return EXIT_SUCCESS;
 }
-
-#ifdef SUPPORT_STEALTH
-/****************************************************************/
-/* Advanced Stealth scan detection Mode One                     */
-/*                                                              */
-/* This mode will see what ports are listening below 1024       */
-/* and will then monitor all the rest. This is very sensitive   */
-/* and will react on any packet hitting any monitored port,     */
-/* regardless of TCP flags set                                  */
-/*                                                              */
-/****************************************************************/
-static int PortSentryAdvancedStealthModeTCP(void) {
-  int result = TRUE, scanDetectTrigger = TRUE, hotPort = TRUE;
-  int openSockfd = 0, smartVerify = FALSE, i;
-  unsigned int incomingPort = 0;
-  unsigned int count = 0, inUsePorts[MAXSOCKS], portCount = 0;
-  char target[IPMAXBUF];
-  char resolvedHost[DNSMAXBUF], *packetType;
-  char packetBuffer[TCPPACKETLEN];
-  struct in_addr addr;
-  struct iphdr *ip;
-  struct tcphdr *tcp;
-
-  Log("adminalert: Advanced mode will monitor first %d ports", configData.tcpAdvancedPort);
-
-  /* try to bind to all ports below 1024, any that are taken we exclude later */
-  for (count = 1; count < configData.tcpAdvancedPort; count++) {
-    if ((openSockfd = OpenTCPSocket()) == ERROR) {
-      Log("adminalert: ERROR: could not open TCP socket. Aborting.");
-      return (ERROR);
-    }
-    if (BindSocket(openSockfd, count) == ERROR)
-      inUsePorts[portCount++] = count;
-
-    close(openSockfd);
-  }
-
-  // FIXME: Don't add duplicate ports in inUsePorts
-  if (configData.tcpAdvancedExcludePortsLength > 0) {
-    for (i = 0; i < configData.tcpAdvancedExcludePortsLength; i++) {
-      inUsePorts[portCount++] = configData.tcpAdvancedExcludePorts[count];
-      Log("Advanced mode will manually exclude port: %d ", inUsePorts[portCount - 1]);
-    }
-  } else {
-    Log("Advanced mode will manually exclude no ports");
-  }
-
-  for (count = 0; count < portCount; count++)
-    Log("adminalert: Advanced Stealth scan detection mode activated. Ignored TCP port: %d", inUsePorts[count]);
-
-  /* open raw socket for reading */
-  if ((openSockfd = OpenRAWTCPSocket()) == ERROR) {
-    Log("adminalert: ERROR: could not open RAW TCP socket. Aborting.");
-    return (ERROR);
-  }
-
-  Log("adminalert: PortSentry is now active and listening.");
-
-  /* main detection loop */
-  for (;;) {
-    if (PacketRead(openSockfd, packetBuffer, TCPPACKETLEN, &ip, (void **)&tcp) != TRUE)
-      continue;
-
-    incomingPort = ntohs(tcp->dest);
-
-    /* don't monitor packets with ACK set (established) or RST */
-    /* This could be a hole in some cases */
-    if ((tcp->ack != 1) && (tcp->rst != 1)) {
-      /* check if we should ignore this connection to this port */
-      for (count = 0; count < portCount; count++) {
-        if ((incomingPort == inUsePorts[count]) || (incomingPort >= configData.tcpAdvancedPort)) {
-          hotPort = FALSE;
-          break;
-        } else {
-          hotPort = TRUE;
-        }
-      }
-
-      if (hotPort) {
-        smartVerify = IsPortInUse(incomingPort, IPPROTO_TCP);
-
-        // FIXME: IsPortInUse returns true, false, error
-        if (smartVerify != TRUE) {
-          addr.s_addr = (u_int)ip->saddr;
-          SafeStrncpy(target, (char *)inet_ntoa(addr), IPMAXBUF);
-          /* check if we should ignore this IP */
-          result = NeverBlock(target, configData.ignoreFile);
-
-          if (result == ERROR) {
-            Log("attackalert: ERROR: cannot open ignore file. Blocking host anyway.");
-            result = FALSE;
-          }
-
-          if (result == FALSE) {
-            /* check if they've visited before */
-            scanDetectTrigger = CheckStateEngine(target);
-
-            if (scanDetectTrigger == TRUE) {
-              if (configData.resolveHost) { /* Do they want DNS resolution? */
-                if (CleanAndResolve(resolvedHost, target) != TRUE) {
-                  Log("attackalert: ERROR: Error resolving host. resolving disabled for this host.");
-                  snprintf(resolvedHost, DNSMAXBUF, "%s", target);
-                }
-              } else {
-                snprintf(resolvedHost, DNSMAXBUF, "%s", target);
-              }
-
-              packetType = ReportPacketType(tcp);
-              Log("attackalert: %s from host: %s/%s to TCP port: %u", packetType, resolvedHost, target, incomingPort);
-              /* Report on options present */
-              if (ip->ihl > 5)
-                Log("attackalert: Packet from host: %s/%s to TCP port: %u has IP options set (detection avoidance technique).",
-                    resolvedHost, target, incomingPort);
-
-              /* check if this target is already blocked */
-              if (IsBlocked(target, configData.blockedFile) == FALSE) {
-                /* toast the prick */
-                if (DisposeTarget(target, incomingPort, IPPROTO_TCP) != TRUE)
-                  Log("attackalert: ERROR: Could not block host %s/%s!!", resolvedHost, target);
-                else
-                  WriteBlocked(target, resolvedHost, incomingPort, configData.blockedFile, configData.historyFile, "TCP");
-              } else { /* end IsBlocked check */
-                Log("attackalert: Host: %s/%s is already blocked Ignoring", resolvedHost, target);
-              }
-            } /* end if(scanDetectTrigger) */
-          }   /* end if(never block) check */
-        }     /* end if(smartVerify) */
-      }       /* end if(hotPort) */
-    }         /* end if(TH_ACK) */
-  }           /* end for( ; ; ) loop */
-}
-/* end PortSentryAdvancedStealthModeTCP */
-
-/****************************************************************/
-/* Advanced Stealth scan detection mode for UDP                 */
-/*                                                              */
-/* This mode will see what ports are listening below 1024       */
-/* and will then monitor all the rest. This is very sensitive   */
-/* and will react on any packet hitting any monitored port.     */
-/* This is a very dangerous option and is for advanced users    */
-/*                                                              */
-/****************************************************************/
-static int PortSentryAdvancedStealthModeUDP(void) {
-  int result = TRUE, scanDetectTrigger = TRUE, hotPort = TRUE;
-  int openSockfd = 0, smartVerify = FALSE, i;
-  unsigned int incomingPort = 0;
-  unsigned int count = 0, inUsePorts[MAXSOCKS], portCount = 0;
-  char target[IPMAXBUF];
-  char resolvedHost[DNSMAXBUF];
-  char packetBuffer[UDPPACKETLEN];
-  struct in_addr addr;
-  struct iphdr *ip;
-  struct udphdr *udp;
-
-  Log("adminalert: Advanced mode will monitor first %d ports", configData.udpAdvancedPort);
-
-  /* try to bind to all ports below 1024, any that are taken we exclude later */
-  for (count = 1; count < configData.udpAdvancedPort; count++) {
-    if ((openSockfd = OpenUDPSocket()) == ERROR) {
-      Log("adminalert: ERROR: could not open UDP socket. Aborting.");
-      return (ERROR);
-    }
-    if (BindSocket(openSockfd, count) == ERROR)
-      inUsePorts[portCount++] = count;
-
-    close(openSockfd);
-  }
-
-  // FIXME: Don't add duplicate ports in inUsePorts
-  if (configData.udpAdvancedExcludePortsLength > 0) {
-    for (i = 0; i < configData.udpAdvancedExcludePortsLength; i++) {
-      inUsePorts[portCount++] = configData.udpAdvancedExcludePorts[count];
-      Log("Advanced mode will manually exclude port: %d ", inUsePorts[portCount - 1]);
-    }
-  } else {
-    Log("Advanced mode will manually exclude no ports");
-  }
-
-  for (count = 0; count < portCount; count++) {
-    Log("adminalert: Advanced Stealth scan detection mode activated. Ignored UDP port: %d", inUsePorts[count]);
-  }
-
-  if ((openSockfd = OpenRAWUDPSocket()) == ERROR) {
-    Log("adminalert: ERROR: could not open RAW UDP socket. Aborting.");
-    return (ERROR);
-  }
-
-  Log("adminalert: PortSentry is now active and listening.");
-
-  /* main detection loop */
-  for (;;) {
-    if (PacketRead(openSockfd, packetBuffer, UDPPACKETLEN, &ip, (void **)&udp) != TRUE)
-      continue;
-
-    incomingPort = ntohs(udp->dest);
-
-    /* check if we should ignore this connection to this port */
-    for (count = 0; count < portCount; count++) {
-      if ((incomingPort == inUsePorts[count]) || (incomingPort >= configData.udpAdvancedPort)) {
-        hotPort = FALSE;
-        break;
-      } else {
-        hotPort = TRUE;
-      }
-    }
-
-    if (hotPort) {
-      smartVerify = IsPortInUse(incomingPort, IPPROTO_UDP);
-
-      // FIXME: IsPortInUse returns true, false, error
-      if (smartVerify != TRUE) {
-        /* copy the clients address into our buffer for nuking */
-        addr.s_addr = (u_int)ip->saddr;
-        SafeStrncpy(target, (char *)inet_ntoa(addr), IPMAXBUF);
-        /* check if we should ignore this IP */
-        result = NeverBlock(target, configData.ignoreFile);
-
-        if (result == ERROR) {
-          Log("attackalert: ERROR: cannot open ignore file. Blocking host anyway.");
-          result = FALSE;
-        }
-
-        if (result == FALSE) {
-          /* check if they've visited before */
-          scanDetectTrigger = CheckStateEngine(target);
-
-          if (scanDetectTrigger == TRUE) {
-            if (configData.resolveHost) { /* Do they want DNS resolution? */
-              if (CleanAndResolve(resolvedHost, target) != TRUE) {
-                Log("attackalert: ERROR: Error resolving host. resolving disabled for this host.");
-                snprintf(resolvedHost, DNSMAXBUF, "%s", target);
-              }
-            } else {
-              snprintf(resolvedHost, DNSMAXBUF, "%s", target);
-            }
-
-            Log("attackalert: UDP scan from host: %s/%s to UDP port: %u", resolvedHost, target, incomingPort);
-            /* Report on options present */
-            if (ip->ihl > 5)
-              Log("attackalert: Packet from host: %s/%s to UDP port: %u has IP options set (detection avoidance technique).", resolvedHost, target, incomingPort);
-
-            /* check if this target is already blocked */
-            if (IsBlocked(target, configData.blockedFile) == FALSE) {
-              if (DisposeTarget(target, incomingPort, IPPROTO_UDP) != TRUE)
-                Log("attackalert: ERROR: Could not block host %s/%s!!", resolvedHost, target);
-              else
-                WriteBlocked(target, resolvedHost, incomingPort, configData.blockedFile, configData.historyFile, "UDP");
-            } else { /* end IsBlocked check */
-              Log("attackalert: Host: %s/%s is already blocked Ignoring", resolvedHost, target);
-            }
-          } /* end if(scanDetectTrigger) */
-        }   /* end if(never block) check */
-      }     /* end if (smartVerify) */
-    }       /* end if(hotPort) */
-  }         /* end for( ; ; ) loop */
-}
-/* end PortSentryAdvancedStealthModeUDP */
-
-#endif
