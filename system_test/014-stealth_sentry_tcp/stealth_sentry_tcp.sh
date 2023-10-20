363ad1306a9be85d61e1c23b0a33c14e8f5e75e0
Marcus Hufvudsson <mh@protohuf.com>
2023-10-20 19:31:02 +0200

    Added stealth trigger check

---
+++ system_test/014-stealth_sentry_tcp/stealth_sentry_tcp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+. ./testlib.sh
+
+verbose "expect connect to tcp localhost:11"
+nmap -sT -p11-11 localhost >/dev/null
+
+confirmBlockTriggered stcp
+
+verbose "expect block anyway when ignore file not found"
+if ! findInFile "^Unable to open ignore file .*/portsentry.ignore. Continuing without it" $PORTSENTRY_STDERR; then
+  err "Expected block anyway message not found"
+fi
+
+verbose "Re-connect to tcp localhost:11"
+nmap -sT -p11-11 localhost >/dev/null
+
+confirmAlreadyBlocked
+
+ok
