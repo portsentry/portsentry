f331a7e5e367445dc49d1ee59bfcb5dc38fb6a49
Marcus Hufvudsson <mh@protohuf.com>
2023-10-20 19:57:01 +0200

    Added advanced test to not trigger above threshold

---
+++ system_test/019-adv_sentry_above_thres_udp/adv_sentry_above_thres_udp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+. ./testlib.sh
+
+runNmap 1025 U
+
+verbose "don't expect attackalert block message"
+if findInFile "^attackalert: Host 127.0.0.1 has been blocked" $PORTSENTRY_STDOUT; then
+  err "No attackalert message expected but was found"
+fi
+
+ok
