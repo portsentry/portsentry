f5666491eb0019eef34ec7249b05a61f466a9547
Marcus Hufvudsson <mh@protohuf.com>
2023-10-26 20:28:44 +0200

    Added convenience script for build testing on a list of remote machines

---
+++ build_remotes.sh
@@ -0,0 +1,33 @@
+#! env bash
+
+BUILD_TYPE="debug"
+
+if [ -n "$1" ]; then
+  BUILD_TYPE="$1"
+
+  if [ "$BUILD_TYPE" != "debug" ] && [ "$BUILD_TYPE" != "release" ]; then
+    echo "Invalid build type: $BUILD_TYPE"
+    exit 1
+  fi
+fi
+
+for host in $(cat remotes.txt); do
+  if [ "$host" == "[LOCAL]" ]; then
+    echo "Building local"
+    ./build.sh clean && ./build.sh debug
+    if [ $? -ne 0 ]; then
+      echo "ERROR: Failed to build local"
+      exit 1
+    fi
+  else
+    echo "Building on $host"
+    rsync -az -e ssh ../portsentry $host:/tmp/
+    ssh $host "cd /tmp/portsentry && ./build.sh clean && ./build.sh $BUILD_TYPE"
+    if [ $? -ne 0 ]; then
+      echo "ERROR: Failed to build on $host"
+      exit 1
+    fi
+  fi
+
+  echo
+done
