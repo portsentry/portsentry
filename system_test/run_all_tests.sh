fddf86fa6c99ddfabc0645debb38f328897eb6e0
Marcus Hufvudsson <mh@protohuf.com>
2023-10-26 21:15:48 +0200

    Removed -not from find since NetBSD doesn't support it

--- system_test/run_all_tests.sh
+++ system_test/run_all_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-for f in $(find . -maxdepth 1 -type d -not -path "\." | sort); do
+for f in $(find . -maxdepth 1 -type d | sort); do
+  if [ "$f" = "." ]; then
+    continue
+  fi
   echo "Running test $f"
   test_script=$(find $f -name "*.sh" -type f |tail -n 1)
   if ! ./run_test.sh ../debug/portsentry $f/portsentry.conf $f/portsentry.test $test_script; then
