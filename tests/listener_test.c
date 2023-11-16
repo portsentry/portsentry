dc390ff8fdb060cb919abc710840606b3d3a5294
Marcus Hufvudsson <mh@protohuf.com>
2023-11-16 10:51:17 +0100

    Initial Unit test program

---
+++ tests/listener_test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "../src/advanced_sentry.h"
+#include "../src/cmdline.h"
+#include "../src/config_data.h"
+#include "../src/configfile.h"
+#include "../src/connect_sentry.h"
+#include "../src/io.h"
+#include "../src/portsentry.h"
+#include "../src/state_machine.h"
+#include "../src/stealth_sentry.h"
+#include "../src/util.h"
+
+int main(int argc, char *argv[]) {
+  ParseCmdline(argc, argv);
+
+  signal(SIGPIPE, SIG_IGN);
+
+  if ((geteuid()) && (getuid()) != 0) {
+    printf("You need to be root to run this.\n");
+    Exit(EXIT_FAILURE);
+  }
+
+  return EXIT_SUCCESS;
+}
