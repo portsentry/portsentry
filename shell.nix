2d9dea7ef1cb6f6e9fd46b2fa78af91bc9cefcff
Marcus Hufvudsson <mh@protohuf.com>
2024-06-11 11:03:00 +0200

    Initial nix shell definition

---
+++ shell.nix
@@ -0,0 +1,18 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    codeql
+    semgrep
+    netcat-gnu
+  ];
+  
+  GREETING = "Welcome to the Portsentry dev environment!";
+
+  shellHook = ''
+    echo $GREETING
+  '';
+}
