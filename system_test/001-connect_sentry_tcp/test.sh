95843f805cc28d44d2cd4be86055c6b1297393cd
Marcus Hufvudsson <mh@protohuf.com>
2024-07-01 14:15:27 +0200

    Fixed escape char

--- system_test/001-connect_sentry_tcp/test.sh
+++ system_test/001-connect_sentry_tcp/test.sh
@@ -13,6 +13,6 @@ runNmap 11 U
 runNmap 11 U
 
 confirmOccurrenceStdout 2 "Scan from: \[127.0.0.1\] (127.0.0.1) protocol: \[TCP\] port: \[11\] type: \[Connect\]"
-confirmOccurrenceStdout 2 "Scan from: \[127.0.0.1\] (127.0.0.1) protocol: \[UDP\] port: \[11\] type: \[Connect]"
+confirmOccurrenceStdout 2 "Scan from: \[127.0.0.1\] (127.0.0.1) protocol: \[UDP\] port: \[11\] type: \[Connect\]"
 
 ok
