52a9e223a7ee9f8c44a2061f2ce24520106cf273
Marcus Hufvudsson <mh@protohuf.com>
2024-09-10 15:03:32 +0200

    Initial fix for IPV6 on linux raw sockets

--- src/sentry_stealth.c
+++ src/sentry_stealth.c
@@ -15,66 +15,50 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <errno.h>
+#include <net/ethernet.h>
+#include <linux/if_packet.h>
+#include <net/if.h>
+#include <string.h>
+#include <sys/ioctl.h>
 
-#include "config_data.h"
-#include "io.h"
 #include "portsentry.h"
-#include "sentry_stealth.h"
+#include "config_data.h"
+#include "packet_info.h"
+#include "io.h"
 #include "util.h"
+
+#define NFDS 2
 
 extern uint8_t g_isRunning;
 
-static int OpenRAWTCPSocket(void);
-static int OpenRAWUDPSocket(void);
-static int OpenRAWTCPSocket6(void);
-static int OpenRAWUDPSocket6(void);
 static int PacketRead(int socket, char *buffer, int bufferLen);
 
 int PortSentryStealthMode(void) {
-  int status = EXIT_FAILURE;
-  int count, nfds, result;
-  int tcpSockfd = -1, udpSockfd = -1;
+  int status = EXIT_FAILURE, result, nfds = NFDS, i;
   char packetBuffer[IP_MAXPACKET], err[ERRNOMAXBUF];
-  struct pollfd fds[4];
+  struct pollfd fds[NFDS];
   struct PacketInfo pi;
 
   assert(configData.sentryMode == SENTRY_MODE_STEALTH);
 
-  nfds = 0;
-  if (configData.tcpPortsLength > 0) {
-    if ((tcpSockfd = OpenRAWTCPSocket()) == ERROR) {
-      goto exit;
-    }
-
-    fds[nfds].fd = tcpSockfd;
-    fds[nfds].events = POLLIN;
-    nfds++;
-
-    if ((tcpSockfd = OpenRAWTCPSocket6()) == ERROR) {
-      goto exit;
-    }
-
-    fds[nfds].fd = tcpSockfd;
-    fds[nfds].events = POLLIN;
-    nfds++;
+  memset(fds, 0, sizeof(fds));
+  for (i = 0; i < nfds; i++) {
+    fds[i].fd = -1;
+    fds[i].events = POLLIN;
   }
 
-  if (configData.udpPortsLength > 0) {
-    if ((udpSockfd = OpenRAWUDPSocket()) == ERROR) {
-      goto exit;
-    }
+  /* Listen for IPv4 and IPv6 packets on different sockets, it will probably(?)
+   * be faster to let the kernel filter out all the other packet types than
+   * using ETH_P_ALL and filter ourselves.
+   */
+  if ((fds[0].fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_IP))) < 0) {
+    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
+    return ERROR;
+  }
 
-    fds[nfds].fd = udpSockfd;
-    fds[nfds].events = POLLIN;
-    nfds++;
-
-    if ((udpSockfd = OpenRAWUDPSocket6()) == ERROR) {
-      goto exit;
-    }
-
-    fds[nfds].fd = udpSockfd;
-    fds[nfds].events = POLLIN;
-    nfds++;
+  if ((fds[1].fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_IPV6))) < 0) {
+    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
+    return ERROR;
   }
 
   Log("PortSentry is now active and listening.");
@@ -92,12 +76,12 @@ int PortSentryStealthMode(void) {
       goto exit;
     }
 
-    for (count = 0; count < nfds; count++) {
-      if (fds[count].revents != POLLIN) {
+    for (i = 0; i < nfds; i++) {
+      if (fds[i].revents != POLLIN) {
         continue;
       }
 
-      if (PacketRead(fds[count].fd, packetBuffer, IP_MAXPACKET) != TRUE)
+      if (PacketRead(fds[i].fd, packetBuffer, IP_MAXPACKET) != TRUE)
         continue;
 
       ClearPacketInfo(&pi);
@@ -127,6 +111,7 @@ int PortSentryStealthMode(void) {
         continue;
       }
 
+      Debug("Packet: %s", GetPacketInfoString(&pi, NULL, -1, -1));
       RunSentry(&pi);
     }
   }
@@ -135,81 +120,41 @@ int PortSentryStealthMode(void) {
 
 exit:
 
-  if (tcpSockfd != -1)
-    close(tcpSockfd);
-
-  if (udpSockfd != -1)
-    close(udpSockfd);
+  for (i = 0; i < nfds; i++) {
+    if (fds[i].fd != -1)
+      close(fds[i].fd);
+  }
 
   return status;
-}
-
-static int OpenRAWTCPSocket(void) {
-  int sockfd;
-  char err[ERRNOMAXBUF];
-
-  Debug("OpenRAWTCPSocket: opening RAW TCP socket");
-
-  if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)) < 0) {
-    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
-    return ERROR;
-  }
-
-  return sockfd;
-}
-
-static int OpenRAWUDPSocket(void) {
-  int sockfd;
-  char err[ERRNOMAXBUF];
-
-  Debug("OpenRAWUDPSocket: opening RAW UDP socket");
-
-  if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) < 0) {
-    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
-    return ERROR;
-  }
-
-  return sockfd;
-}
-
-static int OpenRAWTCPSocket6(void) {
-  int sockfd;
-  char err[ERRNOMAXBUF];
-
-  Debug("OpenRAWTCPSocket: opening RAW IPv6 TCP socket");
-
-  if ((sockfd = socket(AF_INET6, SOCK_RAW, IPPROTO_TCP)) < 0) {
-    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
-    return ERROR;
-  }
-
-  return sockfd;
-}
-
-static int OpenRAWUDPSocket6(void) {
-  int sockfd;
-  char err[ERRNOMAXBUF];
-
-  Debug("OpenRAWUDPSocket: opening RAW IPv6 UDP socket");
-
-  if ((sockfd = socket(AF_INET6, SOCK_RAW, IPPROTO_UDP)) < 0) {
-    Error("Unable to create socket: %s", ErrnoString(err, sizeof(err)));
-    return ERROR;
-  }
-
-  return sockfd;
 }
 
 static int PacketRead(int socket, char *buffer, int bufferLen) {
   char err[ERRNOMAXBUF];
   ssize_t result;
+  struct sockaddr_ll sll;
+  socklen_t sllLen = sizeof(struct sockaddr_ll);
 
-  if ((result = read(socket, buffer, bufferLen)) == -1) {
+  if ((result = recvfrom(socket, buffer, bufferLen, 0, (struct sockaddr *)&sll, &sllLen)) == -1) {
     Error("Could not read from socket %d: %s. Aborting", socket, ErrnoString(err, sizeof(err)));
     return ERROR;
   } else if (result < (ssize_t)sizeof(struct ip)) {
     Error("Packet read from socket %d is too small (%lu bytes). Aborting", socket, result);
     return ERROR;
+  }
+
+  if (sll.sll_pkttype != PACKET_HOST) {
+    Debug("Recived invalid packet on raw socket PacketRead: sllLen: %d, sll_family: %d, sll_protocol: %d (%x), sll_ifindex: %d, sll_hatype: %d, sll_pkttype: %d (%s), sll_halen: %d", sllLen,
+          sll.sll_family, ntohs(sll.sll_protocol), ntohs(sll.sll_protocol), sll.sll_ifindex, sll.sll_hatype, sll.sll_pkttype,
+          (sll.sll_pkttype == PACKET_HOST) ? "PACKET_HOST" : (sll.sll_pkttype == PACKET_BROADCAST) ? "PACKET_BROADCAST"
+                                                         : (sll.sll_pkttype == PACKET_MULTICAST)   ? "PACKET_MULTICAST"
+                                                         : (sll.sll_pkttype == PACKET_OTHERHOST)   ? "PACKET_OTHERHOST"
+                                                         : (sll.sll_pkttype == PACKET_OUTGOING)    ? "PACKET_OUTGOING"
+                                                         : (sll.sll_pkttype == PACKET_LOOPBACK)    ? "PACKET_LOOPBACK"
+                                                         : (sll.sll_pkttype == PACKET_USER)        ? "PACKET_USER"
+                                                         : (sll.sll_pkttype == PACKET_KERNEL)      ? "PACKET_KERNEL"
+                                                                                                   : "UNKNOWN",
+          sll.sll_halen);
+    return FALSE;
   }
 
   return TRUE;
